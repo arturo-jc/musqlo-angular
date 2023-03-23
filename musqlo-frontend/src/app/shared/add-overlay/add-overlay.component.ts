bfa05a9d849aa92611f19513c883c5e34a411cf3
Arturo Javier-Castellanos <arturo@karveit.ca>
2023-03-23 00:33:38 -0500

    Clean up css

--- musqlo-frontend/src/app/shared/add-overlay/add-overlay.component.ts
+++ musqlo-frontend/src/app/shared/add-overlay/add-overlay.component.ts
@@ -5,18 +5,18 @@ import { Component } from '@angular/core';
   selector: 'app-add-overlay',
   templateUrl: './add-overlay.component.html',
   styleUrls: ['./add-overlay.component.scss'],
-      animations: [
-        trigger('animation', [
-          state(
-            'void',
-            style({
-              transform: 'scaleY(0.8)',
-              opacity: 0
-            })
-          ),
-          transition('void => *', animate('.12s cubic-bezier(0, 0, 0.2, 1)')),
-        ])
-    ],
+  animations: [
+    trigger('animation', [
+      state(
+        'void',
+        style({
+          transform: 'scaleY(0.8)',
+          opacity: 0
+        })
+      ),
+      transition('void => *', animate('.12s cubic-bezier(0, 0, 0.2, 1)')),
+    ])
+  ],
 })
 export class AddOverlayComponent {
 
