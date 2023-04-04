805eb4eed6337211e6c501d27c7cf7e92ddef644
Arturo Javier-Castellanos <arturo@karveit.ca>
2023-04-04 00:05:20 -0500

    nvmrc, dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  return res.json({ msg: 'hello world' });
+})
+
+app.listen(3000, () => console.log('Listening...'));
