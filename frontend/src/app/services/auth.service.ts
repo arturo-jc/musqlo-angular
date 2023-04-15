bb31994239e0a7a237c5bb14a80408105a4c7ca2
Arturo Javier-Castellanos <arturo@karveit.ca>
2023-04-15 12:22:32 -0500

    Refactor auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ApolloError, ApolloQueryResult } from '@apollo/client/core';
+import { MutationResult } from 'apollo-angular';
 import { BehaviorSubject, Subject } from 'rxjs';
-import { LogInGQL, SignUpGQL, User } from '../../generated/graphql.generated';
+import { LogInGQL, LogInQuery, SignUpGQL, SignUpMutation, User } from '../../generated/graphql.generated';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  onLogin = new Subject<User>();
-
-  onLogout = new Subject();
-
   private _user = new BehaviorSubject<User | undefined>(undefined);
 
+  private _onAuthSuccess = new Subject<User>();
+
+  private _onAuthFail = new Subject();
+
+  private _onLogOut = new Subject();
+
   user = this._user.asObservable();
+
+  onAuthSuccess = this._onAuthSuccess.asObservable();
+
+  onAuthFail = this._onAuthFail.asObservable();
+
+  onAuthLogout = this._onAuthFail.asObservable();
 
   constructor(
     private signUpGQL: SignUpGQL,
@@ -22,19 +32,29 @@ export class AuthService {
 
   signUp(email: string, password: string, username?: string | null) {
     this.signUpGQL.mutate({ email, password, username })
-      .subscribe(res => {
-        if (!res.data?.signUp) { return; }
-        this._user.next(res.data.signUp);
-        this.onLogin.next(res.data.signUp);
-      });
+    .subscribe({
+      next: res => this.handleSignUpSuccess(res),
+      error: () => this._onAuthFail.next(undefined),
+    });
+  }
+
+  handleSignUpSuccess(res: MutationResult<SignUpMutation>) {
+    if (!res.data?.signUp) { return; }
+    this._user.next(res.data.signUp);
+    this._onAuthSuccess.next(res.data.signUp);
   }
 
   login(email: string, password: string) {
-    this.logInGQL.fetch({ email, password })
-      .subscribe(res => {
-        if (!res.data?.logIn) { return; }
-        this._user.next(res.data.logIn);
-        this.onLogin.next(res.data.logIn);
-      });
+    this.logInGQL.fetch({ email, password }).subscribe({
+      next: res => this.handleLoginSuccess(res),
+      error: () => this._onAuthFail.next(undefined),
+    });
   }
+
+  handleLoginSuccess(res: ApolloQueryResult<LogInQuery>) {
+    if (!res.data?.logIn) { return; }
+    this._user.next(res.data.logIn);
+    this._onAuthSuccess.next(res.data.logIn);
+  }
+
 }
